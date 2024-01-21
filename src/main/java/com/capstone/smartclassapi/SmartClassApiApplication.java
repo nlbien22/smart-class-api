1096866907326a18b230c22fb10232ab59398dfc
Long Bien Nguyen <longbiennguyen@Longs-MacBook-Pro.local>
2024-01-21 22:22:26 +0700

    initial

---
+++ src/main/java/com/capstone/smartclassapi/SmartClassApiApplication.java
@@ -0,0 +1,12 @@
+package com.capstone.smartclassapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartClassApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/capstone/smartclassapi/SmartClassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.smartclassapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
